7967c190aef66c73c7a06d7d6a89a667a5ab62da
Pooria Mon <poriya.monfared@gmail.com>
2022-12-26 14:32:28 +0300

    Article crud ops bugs fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,10 +15,32 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     if @article.save
+      flash[:notice] = "Article was submitted succsefully"
       redirect_to @article
     else
-      render :new, status: unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path, status: :see_other
   end
 
   private
